9637c829980ba3c8e8c18217b933720d7e0ca4d5
lovari <lovari9876@gmail.com>
2020-02-03 10:17:22 +0900

    Revert "dev_con1-1번 추가"
    This reverts commit 61296d316bf00d1d577280520d23b16f9382780e.

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-// 1번 추가링
+// 1번 추가
 
 /**
  * Handles requests for the application home page.
